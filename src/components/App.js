86d234d4f7e3a53c66784b658179141d4243f727
Ranu <O@O.net>
2019-09-11 15:31:48 +0100

    add helper function to improve test coverage

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Gift from './Gift';
+import { max_number } from '../helper/index';
 
 class App extends Component {
 
@@ -13,7 +14,7 @@ class App extends Component {
     addGift = () => {
         const { gifts } = this.state; //destructive construct
         const ids = this.state.gifts.map(gift => gift.id);
-        const max_id =ids.length > 0 ? Math.max(...ids) : 0; //spread operator
+        const max_id = max_number(ids);
 
         gifts.push({ id: max_id + 1 });
 
